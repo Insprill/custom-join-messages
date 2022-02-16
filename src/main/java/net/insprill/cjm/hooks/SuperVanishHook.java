43fae2cec1f56a4b131a3524d89f94d95a590b91
Pierce Thompson <insprill@gmail.com>
2022-02-16 15:47:03 -0500

    Fix possible error from SuperVanish hook

--- src/main/java/net/insprill/cjm/hooks/SuperVanishHook.java
+++ src/main/java/net/insprill/cjm/hooks/SuperVanishHook.java
@@ -3,6 +3,7 @@ package net.insprill.cjm.hooks;
 import de.myzelyam.api.vanish.PlayerVanishStateChangeEvent;
 import net.insprill.cjm.handlers.VanishHandler;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -11,7 +12,10 @@ public class SuperVanishHook implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onVanishToggle(PlayerVanishStateChangeEvent e) {
-        VanishHandler.handleVanishToggle(Bukkit.getPlayer(e.getUUID()), e.isVanishing());
+        Player player = Bukkit.getPlayer(e.getUUID());
+        if (player == null)
+            return; // Why the hell can it return a UUID for a player that doesn't exist??
+        VanishHandler.handleVanishToggle(player, e.isVanishing());
     }
 
 }
