2c1f64eff50ffec34f0e454e621ae7634ce194b1
Pierce Thompson <insprill@gmail.com>
2022-08-27 05:50:57 -0400

    Format Title messages with MineDown

--- src/main/kotlin/net/insprill/cjm/message/types/TitleMessage.kt
+++ src/main/kotlin/net/insprill/cjm/message/types/TitleMessage.kt
@@ -1,15 +1,13 @@
 package net.insprill.cjm.message.types
 
+import de.themoep.minedown.MineDown
 import net.insprill.cjm.message.MessageVisibility
 import net.insprill.cjm.placeholder.Placeholders
 import net.insprill.xenlib.MinecraftVersion
 import net.insprill.xenlib.files.YamlFile
-import net.kyori.adventure.text.minimessage.MiniMessage
-import net.kyori.adventure.title.Title
-import net.kyori.adventure.title.TitlePart
+import net.md_5.bungee.api.chat.BaseComponent
 import org.bukkit.entity.Player
 import java.io.File
-import java.time.Duration
 
 class TitleMessage : MessageType {
 
@@ -18,8 +16,16 @@ class TitleMessage : MessageType {
     override val name = "title"
 
     override fun handle(primaryPlayer: Player, players: List<Player>, rootPath: String?, chosenPath: String, visibility: MessageVisibility) {
-        val title = Placeholders.fillPlaceholders(primaryPlayer, config.getString("$chosenPath.Title", "")!!)
-        val subTitle = Placeholders.fillPlaceholders(primaryPlayer, config.getString("$chosenPath.SubTitle", "")!!)
+        val title = BaseComponent.toLegacyText(
+            *MineDown.parse(
+                Placeholders.fillPlaceholders(primaryPlayer, config.getString("$chosenPath.Title", "")!!)
+            )
+        )
+        val subTitle = BaseComponent.toLegacyText(
+            *MineDown.parse(
+                Placeholders.fillPlaceholders(primaryPlayer, config.getString("$chosenPath.SubTitle", "")!!)
+            )
+        )
         val fadeIn = config.getInt("$chosenPath.Fade-In")
         val stay = config.getInt("$chosenPath.Stay")
         val fadeOut = config.getInt("$chosenPath.Fade-Out")
@@ -34,22 +40,8 @@ class TitleMessage : MessageType {
     }
 
     private fun sendTitle(title: String, subTitle: String, fadeIn: Int, stay: Int, fadeOut: Int, player: Player) {
-        if (config.getBoolean("MiniMessage") && MinecraftVersion.isAtLeast(MinecraftVersion.v1_16_0)) {
-            if (title.isNotEmpty()) {
-                val titleComp = MiniMessage.miniMessage().deserialize(title)
-                player.sendTitlePart(TitlePart.TITLE, titleComp)
-            }
-            if (subTitle.isNotEmpty()) {
-                val subTitleComp = MiniMessage.miniMessage().deserialize(subTitle)
-                player.sendTitlePart(TitlePart.SUBTITLE, subTitleComp)
-            }
-            val times = Title.Times.times(Duration.ofMillis(fadeIn * 50L), Duration.ofMillis(stay * 50L), Duration.ofMillis(fadeOut * 50L))
-            player.sendTitlePart(TitlePart.TIMES, times)
-            println("sent title: " + title)
-        } else {
-            @Suppress("DEPRECATION")
-            player.sendTitle(title, subTitle, fadeIn, stay, fadeOut)
-        }
+        @Suppress("DEPRECATION")
+        player.sendTitle(title, subTitle, fadeIn, stay, fadeOut)
     }
 
     @Suppress("DEPRECATION")
