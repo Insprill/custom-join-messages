33891de3c3104b78925740ace34f79502ee14c55
Pierce Thompson <insprill@gmail.com>
2022-09-09 03:00:31 -0400

    Fix MessageConditions being inverted

--- src/main/kotlin/net/insprill/cjm/message/MessageCondition.kt
+++ src/main/kotlin/net/insprill/cjm/message/MessageCondition.kt
@@ -6,11 +6,11 @@ import org.bukkit.Bukkit
 enum class MessageCondition(private val condition: (MessageType, String) -> Boolean) {
     MAX_PLAYERS({ msg, path ->
         val maxPlayers = msg.config.getOrDefault("$path.Max-Players", -1)
-        maxPlayers < 1 || Bukkit.getOnlinePlayers().size > maxPlayers
+        maxPlayers < 1 || Bukkit.getOnlinePlayers().size < maxPlayers
     }),
     MIN_PLAYERS({ msg, path ->
         val minPlayers = msg.config.getOrDefault("$path.Min-Players", -1)
-        minPlayers < 1 || Bukkit.getOnlinePlayers().size < minPlayers
+        minPlayers < 1 || Bukkit.getOnlinePlayers().size > minPlayers
     }),
     ;
 
