de3b4fd0f7d39516c52171d450c2086dfdedce13
Pierce Thompson <insprill@gmail.com>
2022-09-26 17:08:36 -0400

    Store unsupported message actions instead of supported ones

--- src/main/kotlin/net/insprill/cjm/message/MessageVisibility.kt
+++ src/main/kotlin/net/insprill/cjm/message/MessageVisibility.kt
@@ -1,12 +1,12 @@
 package net.insprill.cjm.message
 
-enum class MessageVisibility(val configSection: String, private vararg val supportedActions: MessageAction) {
-    PUBLIC("Public", MessageAction.FIRST_JOIN, MessageAction.JOIN, MessageAction.QUIT),
-    PRIVATE("Private", MessageAction.FIRST_JOIN, MessageAction.JOIN),
+enum class MessageVisibility(val configSection: String, private vararg val unsupportedActions: MessageAction) {
+    PUBLIC("Public"),
+    PRIVATE("Private", MessageAction.QUIT),
     ;
 
     fun supports(action: MessageAction): Boolean {
-        return supportedActions.contains(action)
+        return !unsupportedActions.contains(action)
     }
 
 }
--- src/test/kotlin/net/insprill/cjm/message/MessageVisibilityTest.kt
+++ src/test/kotlin/net/insprill/cjm/message/MessageVisibilityTest.kt
@@ -1,6 +1,7 @@
 package net.insprill.cjm.message
 
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 
@@ -10,6 +11,18 @@ class MessageVisibilityTest {
     @EnumSource(MessageVisibility::class)
     fun configSection_NotBlank(visibility: MessageVisibility) {
         assertFalse(visibility.configSection.isBlank())
+    }
+
+    @ParameterizedTest
+    @EnumSource(MessageAction::class)
+    fun supports_PublicSupportsAll(action: MessageAction) {
+        assertTrue(MessageVisibility.PUBLIC.supports(action))
+    }
+
+    @ParameterizedTest
+    @EnumSource(MessageAction::class)
+    fun supports_PrivateDoesntSupportQuit(action: MessageAction) {
+        assertTrue(MessageVisibility.PRIVATE.supports(action) == (action != MessageAction.QUIT))
     }
 
 }
