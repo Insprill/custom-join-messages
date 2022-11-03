8d8e012e43632c83378facd6885bdb8eec4a3157
Pierce Thompson <insprill@gmail.com>
2022-11-03 15:01:52 -0400

    Accept a null plugin in EnumUtils#tryGetEnum

--- src/main/kotlin/net/insprill/cjm/util/EnumUtils.kt
+++ src/main/kotlin/net/insprill/cjm/util/EnumUtils.kt
@@ -5,10 +5,10 @@ import kotlin.reflect.KClass
 
 object EnumUtils {
 
-    inline fun <reified T : Enum<T>> tryGetEnum(plugin: Plugin, str: String?, type: KClass<T>): T? {
+    inline fun <reified T : Enum<T>> tryGetEnum(plugin: Plugin?, str: String?, type: KClass<T>): T? {
         val uppercase = str?.uppercase()
         if (uppercase == null || enumValues<T>().none { it.name == uppercase }) {
-            plugin.logger.severe("Unknown ${type.simpleName} '$uppercase'! Please choose from one of the following: ${type.java.enumConstants.contentToString()}")
+            plugin?.logger?.severe("Unknown ${type.simpleName} '$uppercase'! Please choose from one of the following: ${type.java.enumConstants.contentToString()}")
             return null
         }
         return enumValueOf<T>(uppercase)
