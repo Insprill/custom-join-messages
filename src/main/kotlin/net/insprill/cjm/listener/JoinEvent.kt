15e2faef556ca946f44fd870b2750fd53da20eac
Pierce Thompson <insprill@gmail.com>
2022-11-04 15:01:45 -0400

    Remove 10 tick delay on join messages

--- src/main/kotlin/net/insprill/cjm/listener/JoinEvent.kt
+++ src/main/kotlin/net/insprill/cjm/listener/JoinEvent.kt
@@ -5,7 +5,6 @@ import de.themoep.minedown.MineDown
 import net.insprill.cjm.CustomJoinMessages
 import net.insprill.cjm.message.MessageAction
 import net.insprill.cjm.update.UpdateChecker
-import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -18,10 +17,8 @@ class JoinEvent(private val plugin: CustomJoinMessages) : Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     fun onPlayerJoin(e: PlayerJoinEvent) {
         e.joinMessage = ""
-        Bukkit.getScheduler().runTaskLater(plugin, Runnable {
-            val action = if (e.player.hasPlayedBefore()) MessageAction.JOIN else MessageAction.FIRST_JOIN
-            plugin.messageSender.trySendMessages(e.player, action, true)
-        }, 10L)
+        val action = if (e.player.hasPlayedBefore()) MessageAction.JOIN else MessageAction.FIRST_JOIN
+        plugin.messageSender.trySendMessages(e.player, action, true)
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
