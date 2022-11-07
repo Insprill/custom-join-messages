804cb6d967233f90d7f3e8209f151d4e155d8f84
Pierce Thompson <insprill@gmail.com>
2022-11-07 17:19:40 -0500

    Add tests for all placeholders

--- src/test/kotlin/net/insprill/cjm/placeholder/PlaceholderTest.kt
+++ src/test/kotlin/net/insprill/cjm/placeholder/PlaceholderTest.kt
@@ -2,7 +2,10 @@ package net.insprill.cjm.placeholder
 
 import be.seeseemelk.mockbukkit.MockBukkit
 import be.seeseemelk.mockbukkit.ServerMock
+import be.seeseemelk.mockbukkit.entity.PlayerMock
+import net.kyori.adventure.text.Component
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -43,6 +46,89 @@ class PlaceholderTest {
         val player = server.addPlayer()
 
         assertNotNull(placeholder.result.invoke(player))
+    }
+
+    @Test
+    fun displayName_NoCustomName_ReturnsDisplayName() {
+        val player = server.addPlayer()
+        player.displayName(Component.text("Insprill"))
+
+        assertEquals("Insprill", Placeholder.DISPLAY_NAME.result.invoke(player))
+    }
+
+    @Test
+    fun displayName_CustomName_ReturnsCustomName() {
+        val player = server.addPlayer()
+        player.customName(Component.text("Insprill"))
+
+        assertEquals("Insprill", Placeholder.DISPLAY_NAME.result.invoke(player))
+    }
+
+    @Test
+    fun name_HasDisplayName_ReturnsName() {
+        val player = server.addPlayer("player")
+        player.displayName(Component.text("Insprill"))
+
+        assertEquals("player", Placeholder.NAME.result.invoke(player))
+    }
+
+    @Test
+    fun name_HasCustomName_ReturnsName() {
+        val player = server.addPlayer("player")
+        player.customName(Component.text("Insprill"))
+
+        assertEquals("player", Placeholder.NAME.result.invoke(player))
+    }
+
+    @Test
+    fun prefix_NoVault_ReturnsEmptyString() {
+        val player = server.addPlayer()
+
+        assertEquals("", Placeholder.PREFIX.result.invoke(player))
+    }
+
+    @Test
+    fun suffix_NoVault_ReturnsEmptyString() {
+        val player = server.addPlayer()
+
+        assertEquals("", Placeholder.SUFFIX.result.invoke(player))
+    }
+
+    @Test
+    fun uniqueJoins_NewServer_ReturnsZero() {
+        val player = PlayerMock(server, "player")
+
+        assertEquals("0", Placeholder.UNIQUE_JOINS.result.invoke(player))
+    }
+
+    @Test
+    fun uniqueJoins_OnePlayerOnline_ReturnsOne() {
+        val player = server.addPlayer()
+
+        assertEquals("1", Placeholder.UNIQUE_JOINS.result.invoke(player))
+    }
+
+    @Test
+    fun uniqueJoins_MultiplePlayersOnline_ReturnsOne() {
+        server.setPlayers(6)
+        val player1 = server.playerList.onlinePlayers.first()
+
+        assertEquals("6", Placeholder.UNIQUE_JOINS.result.invoke(player1))
+    }
+
+    @Test
+    fun uniqueJoins_OnePlayer_NotOnline_ReturnsOne() {
+        val player = server.addPlayer()
+        player.disconnect()
+
+        assertEquals("1", Placeholder.UNIQUE_JOINS.result.invoke(player))
+    }
+
+    @Test
+    fun uuid_ReturnsCorrectUuid() {
+        val player = server.addPlayer()
+
+        assertEquals(player.uniqueId.toString(), Placeholder.UUID.result.invoke(player))
     }
 
 }
