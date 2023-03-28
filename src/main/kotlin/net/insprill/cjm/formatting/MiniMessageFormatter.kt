3e2756bad989fdb7891a6bb6565b63abd1422bd3
Pierce Thompson <insprill@gmail.com>
2023-03-27 20:00:50 -0400

    Remove isPaper check from MiniMessageFormatter's constructor

--- src/main/kotlin/net/insprill/cjm/formatting/MiniMessageFormatter.kt
+++ src/main/kotlin/net/insprill/cjm/formatting/MiniMessageFormatter.kt
@@ -1,6 +1,5 @@
 package net.insprill.cjm.formatting
 
-import net.insprill.spigotutils.ServerEnvironment
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
 import net.md_5.bungee.api.chat.BaseComponent
@@ -12,7 +11,6 @@ class MiniMessageFormatter : Formatter {
     private val gsonSerializer: GsonComponentSerializer
 
     init {
-        if (!ServerEnvironment.isPaper()) throw IllegalStateException("Cannot use the MiniMessage formatter on Spigot")
         @Suppress("DEPRECATION")
         val unsafe = Bukkit.getUnsafe()
         val clazz = unsafe::class.java
