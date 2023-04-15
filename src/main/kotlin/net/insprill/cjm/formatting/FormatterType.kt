c65609b9428d5358a8701117af462708d956cf2a
Pierce Thompson <insprill@gmail.com>
2023-04-15 09:42:41 -0400

    Don't try to load the MiniMessage formatter on Paper versions older than 1.18.2

--- src/main/kotlin/net/insprill/cjm/formatting/FormatterType.kt
+++ src/main/kotlin/net/insprill/cjm/formatting/FormatterType.kt
@@ -1,13 +1,23 @@
 package net.insprill.cjm.formatting
 
+import net.insprill.spigotutils.MinecraftVersion
 import net.insprill.spigotutils.ServerEnvironment
 
 @Suppress("unused")
-enum class FormatterType(val formatter: Formatter, val prettyName: String, val isCompatible: () -> (CompatibilityResult)) {
+enum class FormatterType(
+    val formatter: Formatter,
+    val prettyName: String,
+    val isCompatible: () -> (CompatibilityResult)
+) {
     MINEDOWN(MinedownFormatter(), "Minedown", { CompatibilityResult.NONE }),
     MINIMESSAGE(
         MiniMessageFormatter(), "MiniMessage",
-        { CompatibilityResult(ServerEnvironment.isPaper(), "You must be running Paper to use the MiniMessage formatter!") }),
+        {
+            CompatibilityResult(
+                ServerEnvironment.isPaper() && MinecraftVersion.isAtLeast(MinecraftVersion.v1_18_2),
+                "You must be running Paper 1.18.2+ to use the MiniMessage formatter!"
+            )
+        }),
     LEGACY(LegacyFormatter(), "Legacy", { CompatibilityResult.NONE });
 
     data class CompatibilityResult(val status: Boolean, val message: String) {
