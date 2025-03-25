e5a3ea569629aa8f86e40517b674df128acb4118
Pierce Thompson <pierce@insprill.net>
2025-03-25 15:49:17 -0400

    Explicitly set Java versions for the paper module

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     kotlin("jvm") version "2.1.10"
 }
@@ -9,4 +11,14 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     implementation("net.insprill:spigot-utils:0.5.0")
+}
+
+tasks {
+    kotlin {
+        // Compile against Java 21, compile to Java 8
+        jvmToolchain(21)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
 }
